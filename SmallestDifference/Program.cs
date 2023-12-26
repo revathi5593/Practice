820956b517a05806215d2a33cb51419326098f86
revathi5593 <154391131+revathi5593@users.noreply.github.com>
2023-12-25 20:02:10 -0500

    Smallest Difference

---
+++ SmallestDifference/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SmallestDifference
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arrayOne = new int[] { -1, 5, 10, 20, 28, 3 };
+			int[] arrayTwo = new int[] { 26, 134, 135, 15, 17 };
+			SmallestDifference(arrayOne, arrayTwo);
+		}
+		public static int[] SmallestDifference(int[] arrayOne, int[] arrayTwo)
+		{
+			// Write your code here.
+			int[] result = new int[2];
+
+			int smallestDiff = int.MaxValue;
+			for (int i = 0; i < arrayOne.Length; i++)
+			{
+				for (int j = 0; j < arrayTwo.Length; j++)
+				{
+					int tempDiff = Math.Abs(arrayOne[i] - arrayTwo[j]);
+					if (tempDiff < smallestDiff) {
+						smallestDiff = tempDiff;
+						result[0] = arrayOne[i];
+						result[1] = arrayTwo[j];
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
