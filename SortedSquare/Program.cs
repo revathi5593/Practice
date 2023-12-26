7e7e26008f61e1e304f17489658337cf9b1bc1b7
revathi5593 <154391131+revathi5593@users.noreply.github.com>
2023-12-25 20:03:28 -0500

    Sorted Square

---
+++ SortedSquare/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedSquare
+{
+	class Program
+	{
+		public int[] SortedSquaredArray(int[] array)
+		{
+			List<int> result = new List<int>();
+			foreach (int i in array)
+			{
+				result.Add(i * i);
+			}
+			if (result.Count > 0)
+			{
+				result.Sort();
+				return result.ToArray();
+			}
+			else
+				return new int[] { };
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
