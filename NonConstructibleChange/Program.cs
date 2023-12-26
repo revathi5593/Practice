1a4c8de2bb5a033c6896c4203bd4d4a09d337fda
revathi5593 <154391131+revathi5593@users.noreply.github.com>
2023-12-25 20:02:55 -0500

    Non Constructible Change

---
+++ NonConstructibleChange/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NonConstructibleChange
+{
+	class Program
+	{
+		public static int NonConstructibleChange(int[] coins)
+		{
+			Array.Sort(coins);
+			int minimum = 0;
+			if (coins.Length == 0 || coins[0] > 1)
+				return 1;
+			for (int i = 0; i < coins.Length; i++)
+			{
+				if (minimum + 1 < coins[i])
+					minimum += coins[i];
+				else
+					return minimum;
+
+				//if (coins[i] > minimum + 1)
+				//	return minimum + 1;
+				//minimum += coins[i];
+			}
+			return minimum + 1;
+		}
+		static void Main(string[] args)
+		{
+			int[] input = new int[] { 5, 7, 1, 1, 2, 3, 22 };
+			NonConstructibleChange(input);
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
