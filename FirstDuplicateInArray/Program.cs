29e5712e4498d6046081aa1b6e12c5977e7e4556
revathi5593 <154391131+revathi5593@users.noreply.github.com>
2024-01-21 19:32:09 -0500

    Add files via upload

---
+++ FirstDuplicateInArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static int FirstDuplicateValue(int[] array)
+    {
+        HashSet<int> seen = new HashSet<int>();
+
+        foreach (int num in array)
+        {
+            if (seen.Contains(num))
+            {
+                return num; // First duplicate found
+            }
+
+            seen.Add(num);
+        }
+
+        return -1; // No duplicates found
+    }
+
+    public static void Main()
+    {
+        int[] array = { 1, 3, 4, 2, 2, 5 };
+
+        int result = FirstDuplicateValue(array);
+
+        Console.WriteLine($"First duplicate: {result}");
+    }
+}
---
+++ LongestPeak/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LongestPeak
+{
+	class Program
+	{
+        public static int LongestPeak(int[] array)
+        {
+            int longestPeakLength = 0;
+            int i = 1; // Start from index 1 to avoid checking peaks at the edges
+
+            while (i < array.Length - 1)
+            {
+                bool isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
+
+                if (!isPeak)
+                {
+                    i++;
+                    continue;
+                }
+
+                int leftIdx = i - 2;
+                while (leftIdx >= 0 && array[leftIdx] < array[leftIdx + 1])
+                {
+                    leftIdx--;
+                }
+
+                int rightIdx = i + 2;
+                while (rightIdx < array.Length && array[rightIdx] < array[rightIdx - 1])
+                {
+                    rightIdx++;
+                }
+
+                int currentPeakLength = rightIdx - leftIdx - 1;
+                longestPeakLength = Math.Max(longestPeakLength, currentPeakLength);
+
+                i = rightIdx;
+            }
+
+            return longestPeakLength;
+        }
+
+        static void Main(string[] args)
+		{
+            int[] array = { 1,2,3,3,4,0,10,6,5,-1,-3,2,3 };
+            int result = LongestPeak(array);
+            Console.WriteLine("Hello World!");
+		}
+	}
+}
