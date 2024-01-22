313f263638e0035483e0120ae185b9bee972edaf
revathi5593 <154391131+revathi5593@users.noreply.github.com>
2024-01-21 19:31:33 -0500

    Add files via upload

---
+++ ArrayOfProducts/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayOfProducts
+{
+	class Program
+	{
+		public int[] ArrayOfProducts(int[] array)
+		{
+			int[] result = new int[array.Length];
+			int productLeft = 1;
+			for (int i = 0; i < array.Length; i++)
+			{
+				result[i] = productLeft;
+				productLeft *= array[i];
+				 			
+			}
+			int productRight = 1;
+			for (int i = array.Length -1 ; i >=0 ; i--)
+			{
+				result[i] *= productRight;
+				productRight *= array[i];
+
+			}
+			return result;
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
