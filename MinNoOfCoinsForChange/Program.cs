9f9c8f100314f3ea22d3d338d2d352ad569b4008
revathi5593 <154391131+revathi5593@users.noreply.github.com>
2024-04-21 20:39:37 -0400

    Add files via upload

---
+++ MinNoOfCoinsForChange/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MinNoOfCoinsForChange
+{
+	class Program
+	{
+        public static int MinNumberOfCoinsForChange(int n, int[] denoms)
+        {
+            int[] dp = new int[n + 1];
+            Array.Fill(dp, n + 1); // Initialize with n + 1 (maximum possible coins needed + 1)
+
+            dp[0] = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                foreach (int denom in denoms)
+                {
+                    if (denom <= i)
+                    {
+                        dp[i] = Math.Min(dp[i], dp[i - denom] + 1);
+                    }
+                }
+            }
+
+            return dp[n] > n ? -1 : dp[n]; // If dp[n] is greater than n, it means it's impossible
+        }
+        static void Main(string[] args)
+		{
+            int[] input = new int[] { 1, 5, 10 };
+            MinNumberOfCoinsForChange(7,input);
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ ReConstructBST/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+public class Program
+{
+    // This is an input class. Do not edit.
+    public class BST
+    {
+        public int value;
+        public BST left = null;
+        public BST right = null;
+
+        public BST(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    public BST ReconstructBst(List<int> preOrderTraversalValues)
+    {
+        return ConstructBST(preOrderTraversalValues, 0, preOrderTraversalValues.Count - 1);
+    }
+
+    private BST ConstructBST(List<int> preOrderTraversalValues, int start, int end)
+    {
+        if (start > end)
+        {
+            return null;
+        }
+
+        int rootValue = preOrderTraversalValues[start];
+        BST root = new BST(rootValue);
+
+        int leftSubtreeEnd = start;
+        for (int i = start + 1; i <= end; i++)
+        {
+            if (preOrderTraversalValues[i] < rootValue)
+            {
+                leftSubtreeEnd++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        root.left = ConstructBST(preOrderTraversalValues, start + 1, leftSubtreeEnd);
+        root.right = ConstructBST(preOrderTraversalValues, leftSubtreeEnd + 1, end);
+
+        return root;
+    }
+}
