6c2129b856d3136b6af121dd9ccdba2662408463
revathi5593 <154391131+revathi5593@users.noreply.github.com>
2024-04-21 20:39:14 -0400

    Add files via upload

---
+++ FindKthElement/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    // This is an input class. Do not edit.
+    public class BST
+    {
+        public int value;
+        public BST left = null;
+        public BST right = null;
+
+        public BST(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    public int FindKthLargestValueInBst(BST tree, int k)
+    {
+        Stack<BST> stack = new Stack<BST>();
+        BST current = tree;
+
+        // Traverse the tree in reverse in-order (right -> root -> left)
+        while (current != null || stack.Count > 0)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.right;
+            }
+
+            current = stack.Pop();
+            k--;
+
+            if (k == 0)
+            {
+                return current.value; // Found the kth largest value
+            }
+
+            current = current.left;
+        }
+
+        // If k is greater than the number of nodes in the tree
+        throw new ArgumentException("k is greater than the number of nodes in the tree");
+    }
+}
---
+++ MinHeightBST/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static BST MinHeightBst(List<int> array)
+    {
+        // Base case: If the array is empty, return null
+        if (array.Count == 0)
+        {
+            return null;
+        }
+
+        // Find the middle index of the array
+        int middleIndex = array.Count / 2;
+
+        // Create the root node of the BST
+        BST root = new BST(array[middleIndex]);
+
+        // Recursively build the left and right subtrees
+        root.left = MinHeightBst(array.GetRange(0, middleIndex));
+        root.right = MinHeightBst(array.GetRange(middleIndex + 1, array.Count - middleIndex - 1));
+
+        return root;
+    }
+
+    public class BST
+    {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value)
+        {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+
+        public void insert(int value)
+        {
+            if (value < this.value)
+            {
+                if (left == null)
+                {
+                    left = new BST(value);
+                }
+                else
+                {
+                    left.insert(value);
+                }
+            }
+            else
+            {
+                if (right == null)
+                {
+                    right = new BST(value);
+                }
+                else
+                {
+                    right.insert(value);
+                }
+            }
+        }
+    }
+}
