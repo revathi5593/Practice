9cfa6c26d2c088ba86931ade54d8887d437d21bd
revathi5593 <154391131+revathi5593@users.noreply.github.com>
2024-04-21 20:23:54 -0400

    Add files via upload

---
+++ BSTTraversal/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static List<int> InOrderTraverse(BST tree, List<int> array)
+    {
+        InOrderTraverseInternal(tree, array);        
+        return array;
+    }
+    public static void InOrderTraverseInternal(BST tree, List<int> array)
+    {
+        if (tree != null)
+        {
+            InOrderTraverse(tree.left, array);
+            array.Add(tree.value);
+            InOrderTraverse(tree.right, array);
+        }
+    }
+
+    public static List<int> PreOrderTraverse(BST tree, List<int> array)
+    {
+        PreOrderTraverseInternal(tree, array);
+        return array;
+    }
+    public static void PreOrderTraverseInternal(BST tree, List<int> array)
+    {
+        if (tree != null)
+        {
+            array.Add(tree.value);
+            PreOrderTraverse(tree.left, array);
+            PreOrderTraverse(tree.right, array);
+        }
+    }
+
+    public static List<int> PostOrderTraverse(BST tree, List<int> array)
+    {
+        PostOrderTraverseInternal(tree, array);
+        return array;
+    }
+    public static void PostOrderTraverseInternal(BST tree, List<int> array)
+    {
+        if (tree != null)
+        {
+            PostOrderTraverseInternal(tree.left, array);
+            PostOrderTraverseInternal(tree.right, array);
+            array.Add(tree.value);
+        }
+    }
+
+    public class BST
+    {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value)
+        {
+            this.value = value;
+        }
+    }
+}
---
+++ ValidateBST/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    public static bool ValidateBst(BST tree)
+    {
+        return ValidateBstNew(tree, int.MinValue, int.MaxValue);
+    }
+
+    private static bool ValidateBstNew(BST tree, int minValue, int maxValue)
+    {
+        if (tree == null)
+            return true;
+        // Check if the current node value is within the valid range
+        if (tree.value <= minValue || tree.value >= maxValue)
+            return false;
+        
+
+        // Recursively check left and right subtrees
+        return ValidateBstNew(tree.left, minValue, tree.value) &&
+               ValidateBstNew(tree.right, tree.value, maxValue);
+    }
+
+    public class BST
+    {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value)
+        {
+            this.value = value;
+        }
+    }
+}
